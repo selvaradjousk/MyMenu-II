d8d39b1ad7a6d964340e0798c3fe24b414149ecb
selvaradjousk <senthil.selvaradjou@gmail.com>
2020-11-21 22:39:19 +0100

    using switch statements instead of if statements

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -16,14 +16,19 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-        if (nbMenu == 1) {
-            System.out.println("Vous avez choisi comme menu : poulet");
-        } else if (nbMenu == 2) {
-            System.out.println("Vous avez choisi comme menu : boeuf");
-        } else if (nbMenu == 3) {
-            System.out.println("Vous avez choisi comme menu : végétarien");
-        } else {
-            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        switch (nbMenu) {
+            case 1:
+                System.out.println("Vous avez choisi comme menu : poulet");
+                break;
+            case 2:
+                System.out.println("Vous avez choisi comme menu : boeuf");
+                break;
+            case 3:
+                System.out.println("Vous avez choisi comme menu : végétarien");
+                break;
+            default:
+                System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+                break;
         }
     }
 }
